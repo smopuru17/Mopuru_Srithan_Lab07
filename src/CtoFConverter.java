cd1163e9673b7c34f3f02061aaa22944d82fe659
smopu <mopuruss@mail.uc.edu>
2023-10-21 21:35:13 -0400

    Final

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,20 +1,34 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class CtoFConverter
 
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) {
 
-    {
-
-
-
-
+        Scanner in = new Scanner(System.in);
+        String trash = "";
+        Boolean done = false;
 
 
+        do
+        {
+            System.out.print("Enter Temperature in Celcius: ");
+            if (in.hasNextDouble())
+            {
 
+                double cel = in.nextDouble();
+                double fahren = ((cel * 9/5) + 32);
+                System.out.println("Your temperature in Fahrenheit: " + fahren);
+                done = true;
+            }
+            else
+            {
+            trash = in.nextLine();
+            System.out.println("You entered an invalid statement: " + trash);
+            System.out.println("Try again");
+            }
 
-
+        }while(!done);
 
     }
 }
